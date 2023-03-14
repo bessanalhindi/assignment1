a0960b44570fa7d9282b455f232941093e32e8b5
bessanalhindi <bselhinde@gmail.com>
2023-03-15 01:04:40 +0200

    bessan

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,24 +1,81 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.Query;
 
 public class MainActivity extends AppCompatActivity {
-Button btnsave;
-EditText name;
+    FloatingActionButton addNoteBtn;
+    RecyclerView recyclerView;
+    ImageButton menuBtn;
+    NoteAdapter noteAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addNoteBtn = findViewById(R.id.add_note_btn);
+        recyclerView = findViewById(R.id.recyler_view);
+        menuBtn = findViewById(R.id.menu_btn);
 
-        btnsave=findViewById(R.id.btn_save);
-        name=findViewById(R.id.et_user_name);
+        addNoteBtn.setOnClickListener((v)-> startActivity(new Intent(MainActivity.this,Notedetails.class)) );
+      //  menuBtn.setOnClickListener((v)->showMenu() );
+        //setupRecyclerView();
     }
-    public void saveToFirebase(View v){
 
-    }
+//    void showMenu(){
+//        PopupMenu popupMenu  = new PopupMenu(MainActivity.this,menuBtn);
+//        popupMenu.getMenu().add("Logout");
+//        popupMenu.show();
+//        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+//            @Override
+//            public boolean onMenuItemClick(MenuItem menuItem) {
+//                if(menuItem.getTitle()=="Logout"){
+//                    FirebaseAuth.getInstance().signOut();
+//                    startActivity(new Intent(MainActivity.this,LoginActivity.class));
+//                    finish();
+//                    return true;
+//                }
+//                return false;
+//            }
+//        });
+//
+//    }
+//
+//    void setupRecyclerView(){
+//        Query query  = Utility.getCollectionReferenceForNotes().orderBy("timestamp", Query.Direction.DESCENDING);
+//        FirestoreRecyclerOptions<Note> options = new FirestoreRecyclerOptions.Builder<Note>()
+//                .setQuery(query,Note.class).build();
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        noteAdapter = new NoteAdapter(options,this);
+//        recyclerView.setAdapter(noteAdapter);
+//    }
+//
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        noteAdapter.startListening();
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        noteAdapter.stopListening();
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        noteAdapter.notifyDataSetChanged();
+//    }
 }
+
